b7a335b1ee8861b9258706d6737e66db6ad60eb2
johnnyfsa <joaofeirense@gmail.com>
2024-05-26 22:07:34 -0300

    first commit

---
+++ src/main/java/br/com/biscoithor/FipeTable/FipeTableApplication.java
@@ -0,0 +1,13 @@
+package br.com.biscoithor.FipeTable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/biscoithor/FipeTable/FipeTableApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.biscoithor.FipeTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
